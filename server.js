a6a47e6c126c916df123ff5db1aaa0590dfbe97b
cntbabul <cntbabul@gmail.com>
2024-06-22 18:00:39 +0530

    init

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
+
 //dot config
 dotenv.config();
 
@@ -25,6 +27,15 @@ app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//static folder
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //port
 const PORT = process.env.PORT || 8000;
